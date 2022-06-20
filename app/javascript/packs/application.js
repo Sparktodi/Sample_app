8425e47c1fce56210e4604f16bb224166f90390c
Sparktodi <vitalik.komik@mail.ru>
2022-06-20 20:20:44 +0300

    Hello

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,27 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bootstrap'
+import './src/application.scss'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels").start()
+require("jquery").start()
+require("bootstrap-sprockets").start()
+require("jquery_ujs").start()
+require("require_tree .").start()
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
